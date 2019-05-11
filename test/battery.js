509ba701d4a1c26f794459e4eff2b9ece92c75ca
Martín M <skydiver@users.noreply.github.com>
2019-05-11 19:02:09 -0300

    added remaing battery tests

--- test/battery.js
+++ test/battery.js
@@ -17,4 +17,30 @@ describe('Battery', function() {
       expect(result).to.be.include('%');
     });
   });
+
+  describe('Keyboard', function() {
+    it('should return a number', async function() {
+      const result = await battery.keyboard({ P: false });
+      expect(result).to.be.an('number');
+    });
+
+    it('should return string', async function() {
+      const result = await battery.keyboard({ P: true });
+      expect(result).to.be.an('string');
+      expect(result).to.be.include('%');
+    });
+  });
+
+  describe('Trackpad', function() {
+    it('should return a number', async function() {
+      const result = await battery.trackpad({ P: false });
+      expect(result).to.be.an('number');
+    });
+
+    it('should return string', async function() {
+      const result = await battery.trackpad({ P: true });
+      expect(result).to.be.an('string');
+      expect(result).to.be.include('%');
+    });
+  });
 });
