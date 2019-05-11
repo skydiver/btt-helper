368a03d78eacc6771d825fd1716cc7072be3c4d6
Martín M <skydiver@users.noreply.github.com>
2019-05-11 20:09:31 -0300

    refactor brew command + tests

--- bin/btt-helper-brew.js
+++ bin/btt-helper-brew.js
@@ -1,21 +1,21 @@
 const program = require('commander');
 const pkg = require('../package.json');
 const brew = require('../commands/brew');
-const helpers = require('../lib/helpers');
+const { handleError } = require('../lib/helpers');
 
-program
-  .version(pkg.version);
+program.version(pkg.version);
 
 program
   .command('services')
   .description('Display total running services')
-  .action(query => brew
-    .services(query)
-    .catch(helpers.handleError)
+  .action(query =>
+    brew
+      .services(query)
+      .then(console.log)
+      .catch(handleError)
   );
 
-program
-  .parse(process.argv);
+program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
